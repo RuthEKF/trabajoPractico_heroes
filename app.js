d35d19845b09eb3fc777fbc4885b3e0819cd6781
RuthEKF <ruthcabrerafranco043@gmail.com>
2021-07-29 01:51:36 -0300

    Subo las vistas faltante

--- app.js
+++ app.js
@@ -14,6 +14,22 @@ app.get ('/babbage', (req, res) =>
 app.get ('/clarke', (req, res) => 
     res.sendFile(path.join(__dirname, 'views', 'clarke.html'))
 )
-
+app.get ('/hamilton', (req, res) => 
+    res.sendFile(path.join(__dirname, 'views', 'hamilton.html'))
+)
+app.get ('/hopper', (req, res) => 
+    res.sendFile(path.join(__dirname, 'views', 'hopper.html'))
+)
+app.get ('/lovelace', (req, res) => 
+    res.sendFile(path.join(__dirname, 'views', 'lovelace.html'))
+)
+app.get ('/berners', (req, res) => 
+    res.sendFile(path.join(__dirname, 'views', 'berners-lee.html'))
+)
+app.get ('/turing', (req, res) => 
+    res.sendFile(path.join(__dirname, 'views', 'turing.html'))
+)
 
 const port = 3030
+
+app.listen (port, () => console.log ('Levante el servidor en http://localhost:' + port));
